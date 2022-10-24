225ea8ffb5694f039c3119412cd74bba3b48aa37
Emanuel Maia <tavanomanu@gmail.com>
2022-10-24 18:44:14 +0100

    Bit more done in pg4 (really don't have the time or patience atm)

---
+++ pg4/uglynumber.py
@@ -0,0 +1,9 @@
+def rec_factor(x, y):
+    d = {True : lambda x, y : x, False : lambda a, b : rec_factor(a / b, b)}
+    return d[x % y != 0](x, y)
+
+def ugly(n):
+    return True * (rec_factor(rec_factor(rec_factor(n, 2), 3), 5) == 1)
+
+
+print(ugly(8))
